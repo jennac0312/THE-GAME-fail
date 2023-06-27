6340fcfcd6d10d48db9c58841a273de4c9f4f5f7
Jenna C <jennac0312@gmail.com>
2023-06-27 16:59:15 -0400

    added mode screen

--- FINAL2/app.js
+++ FINAL2/app.js
@@ -1,15 +1,17 @@
 // screens
 const homePage = document.querySelector('.landingScreen')
 const directionsPage = document.querySelector('.directionsScreen')
+const modeScreen = document.querySelector('.modeScreen')
 const gamePage = document.querySelector('.gameBoard')
+
+// home page variables
+const gameTitle = document.querySelector('.title')
+const hpParagraph = document.querySelector('.landingScreen p')
+const hpBContainer = document.querySelector('.buttons')
 const buttons = document.querySelectorAll('button')
 
-// console.log(buttons)
 
-// game title
-const gameTitle = document.querySelector('.title')
-console.log(gameTitle)
-
+// object to track screen location
 
 
 
@@ -17,15 +19,21 @@ console.log(gameTitle)
 // toggle screens for buttons
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+
         // hide home page
         homePage.classList.add('hidden')
-        // hide game and directions
+
+        // hide other screens ( game and directions and modes )
+            // make this a looop... DRY
+        modeScreen.classList.add('hidden')
         gamePage.classList.add('hidden')
         directionsPage.classList.add('hidden')
+
         if(button.classList.value.includes('start')){
             console.log('start button clicked')
-            // show game screen
-            gamePage.classList.remove('hidden')
+            // show mode screen
+            modeScreen.classList.remove('hidden')
+
         } else if (button.classList.contains('directions')){
             console.log(`directions clicked`)
             // show directions
@@ -39,3 +47,11 @@ buttons.forEach((button) => {
 setTimeout(() => {
     gameTitle.setAttribute('style', 'animation: flashing 1s infinite;')
 }, 10000)
+// 12 second delay... show buttons
+setTimeout(() => {
+    hpBContainer.classList.remove('hidden')
+}, 12000)
+// 13 second delay... show hpParagraph
+setTimeout(() => {
+    hpParagraph.style.color = `moccasin`
+}, 13500)
